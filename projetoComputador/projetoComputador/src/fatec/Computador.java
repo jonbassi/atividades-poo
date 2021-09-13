87cf5e9852baaf93255edf8bb9e9e8608e5f35ee
Jon <jonbassi64@outlook.com>
2021-09-13 18:49:05 -0300

    subindo novos arquivos

---
+++ projetoComputador/projetoComputador/src/fatec/Computador.java
@@ -0,0 +1,23 @@
+package fatec;
+
+public class Computador {
+
+    public String marca;
+    public String cor;
+    public String modelo;
+    public long numeroSerie;
+    public double preco;
+
+    public void imprimir(){
+
+    }
+
+    public void calcularValor(){
+
+    }
+
+    public int alterarValor(){
+
+    }
+
+}
--- projetoComputador/projetoComputador/src/fatec/ComputadorTeste.java
+++ projetoComputador/projetoComputador/src/fatec/ComputadorTeste.java
@@ -1,6 +1,6 @@
 package fatec;
 
-public class Main {
+public class ComputadorTeste {
 
     public static void main(String[] args) {
 	// write your code here
