ee631ad79a7dc4a0e32a731b5e72ca43e2ab13fd
Jon <jonbassi64@outlook.com>
2021-09-14 17:15:05 -0300

    iniciando exercicio3

---
+++ projetoConta/src/Fatec/Conta.java
@@ -0,0 +1,4 @@
+package Fatec;
+
+public class Conta {
+}
---
+++ projetoConta/src/Fatec/ContaTeste.java
@@ -0,0 +1,8 @@
+package Fatec;
+
+public class ContaTeste {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
