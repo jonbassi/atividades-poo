20ae4711b08d1b28799222dcb0d93fc5cb30bab2
Jon <jonbassi64@outlook.com>
2021-09-14 17:09:56 -0300

    subindo exercicio2

--- projetoComputador/projetoComputador/src/fatec/Computador.java
+++ projetoComputador/projetoComputador/src/fatec/Computador.java
@@ -1,5 +1,7 @@
 package fatec;
 
+import java.util.Scanner;
+
 public class Computador {
 
     public String marca;
@@ -9,14 +11,32 @@ public class Computador {
     public double preco;
 
     public void imprimir(){
-
+        System.out.println("Marca: "+marca);
+        System.out.println("Cor: "+cor);
+        System.out.println("Modelo: "+modelo);
+        System.out.println("Número de série: "+numeroSerie);
+        System.out.println("Preço: "+preco);
     }
 
     public void calcularValor(){
-
+        if(marca == "HP"){
+            preco += (preco * 30)/100;
+        }else{
+            if(marca == "IBM"){
+                preco += (preco * 50)/100;
+            }
+        }
     }
 
-    public int alterarValor(){
+    public int alterarValor(int valor){
+        preco += valor;
+        if(valor >0){
+            System.out.println("Valor alterado");
+            return 1;
+        }else{
+            System.out.println("Valor não alterado");
+            return 0;
+        }
 
     }
 
--- projetoComputador/projetoComputador/src/fatec/ComputadorTeste.java
+++ projetoComputador/projetoComputador/src/fatec/ComputadorTeste.java
@@ -1,8 +1,36 @@
 package fatec;
 
+import java.util.Scanner;
+
 public class ComputadorTeste {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Computador computadorHP = new Computador();
+
+        computadorHP.marca = "HP";
+
+        computadorHP.imprimir();
+        computadorHP.calcularValor();
+        computadorHP.imprimir();
+
+        Computador computadorIBM = new Computador();
+
+        computadorIBM.marca = "IBM";
+
+        computadorIBM.imprimir();
+        computadorIBM.calcularValor();
+        computadorIBM.imprimir();
+
+
+        computadorIBM.alterarValor(computadorIBM.alterarValor(1));
+
+
+
+
+
+
+
+
     }
 }
